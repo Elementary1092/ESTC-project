0e0f2dfeed4fbecd50d2e4cccf84f25946dd22d6
Elementary1092 <pdead6223@gmail.com>
2022-11-01 15:08:24 +0000

    rewrite main

--- main.c
+++ main.c
@@ -1,48 +1,48 @@
 #include <stdbool.h>
-#include <stdint.h>
+#include <string.h>
 
-#include "blink/controller.h"
+#include "blink/button_controller.h"
+#include "blink/led_controller.h"
 #include "nrf_delay.h"
-#include "boards.h"
 
-#define LED_INVERT_DELAY 500
-#define ITERATION_DELAY_TIME 3000
+#define SW1_PORT 1
+#define SW1_PIN 6
 
-#define BOARD_ID_REVERSED 8917
+#define BLINK_SEQUENCE "RGBBGRRRGGBB"
+
+#define LED_ON_TIME  750
+#define LED_OFF_TIME 250
 
 /**
  * @brief Function for application main entry.
  */
 int main(void)
 {
-    /* Configure board. */
-    bsp_board_init(BSP_INIT_LEDS);
+    /* Initializing SW1 button */
+    struct button_controller_t button;
+    init_button_controller(&button, SW1_PORT, SW1_PIN);
 
-    /* Initialize blink time controller */
-    struct time_controller_t c;
-    init_time_controller(&c, BOARD_ID_REVERSED);
+    /* Initializing leds and setting blinking sequence */
+    struct led_controller_t leds;
+    init_led_controller(&leds, BLINK_SEQUENCE, ((uint8_t)strlen(BLINK_SEQUENCE)));
 
     /* Toggle LEDs. */
     while (true)
     {
-        for (uint8_t i = next(&c); i > 0; i--)
+        while (is_pressed(&button))
         {
-            for (int j = 0; j < LEDS_NUMBER; j++) 
+            switch_on_next_led(&leds);
+            nrf_delay_ms(LED_ON_TIME);
+
+            if (is_pressed(&button))
             {
-                bsp_board_led_invert(j);
-                nrf_delay_ms(LED_INVERT_DELAY);
-                bsp_board_led_invert(j);
-            }
-            if (i == 1) 
-            {
-                bsp_board_leds_off();
+                switch_off_led(&leds);
+                nrf_delay_ms(LED_OFF_TIME);
             }
         }
-
-        nrf_delay_ms(ITERATION_DELAY_TIME);
     }
 
-    destruct_time_controller(&c);
+    free_led_controller(&leds);
 }
 
 /**
