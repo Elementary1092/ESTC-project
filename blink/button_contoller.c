120c1a73e4d4dd34e997f2cddcafb500280c4fa3
Elementary1092 <pdead6223@gmail.com>
2022-11-01 14:23:49 +0000

    feat: add led controller change button controller resistor

--- blink/button_contoller.c
+++ blink/button_contoller.c
@@ -6,14 +6,14 @@ void init_button_controller(button_controller_t *c, int port, int pin)
 	NRFX_ASSERT(c != NULL);
 	NRFX_ASSERT(port >= 0 && port < 2);
 	NRFX_ASSERT(pin >= 0 && port < 32);
-	
+
 	c->gpio_pin = NRF_GPIO_PIN_MAP(port, pin);
 
-	nrf_gpio_cfg_input(c->gpio_pin, NRF_GPIO_PIN_PULLDOWN);
+	nrf_gpio_cfg_input(c->gpio_pin, NRF_GPIO_PIN_PULLUP);
 }
 
 uint32_t is_pressed(button_controller_t *c)
 {
 	NRFX_ASSERT(c != NULL);
-	return nrf_gpio_pin_read(c->gpio_pin);
+	return !(nrf_gpio_pin_read(c->gpio_pin));
 }
---
+++ blink/led_controller.c
@@ -0,0 +1,65 @@
+#include <hal/nrf_gpio.h>
+#include <malloc.h>
+#include "led_controller.h"
+
+#define SWITCH_LED_ON_SIGNAL 0
+#define SWITCH_LED_OFF_SIGNAL 1
+
+void init_led_controller(led_controller_t *c, const char *sequence, uint8_t nseq)
+{
+	NRFX_ASSERT(c != NULL);
+	NRFX_ASSERT(sequence != NULL);
+
+	/* Getting gpio pin number of red, green and blue leds */
+	uint32_t red = NRF_GPIO_PIN_MAP(0, 8);
+	uint32_t green = NRF_GPIO_PIN_MAP(1, 9);
+	uint32_t blue = NRF_GPIO_PIN_MAP(0, 12);
+
+	/* Configuring these pins as output */
+	nrf_gpio_cfg_output(red);
+	nrf_gpio_cfg_output(green);
+	nrf_gpio_cfg_output(blue);
+
+	/* Initializing leds blink sequence */
+	c->gpio_pin_sequence = malloc(((size_t)nseq) * sizeof(uint32_t));
+	c->pin_idx = 0;
+	uint8_t sequence_last_idx = 0;
+	for (uint8_t i = 0; i < nseq; i++)
+	{
+		switch (sequence[i])
+		{
+		case 'R':
+			c->gpio_pin_sequence[sequence_last_idx++] = red;
+			break;
+		case 'G':
+			c->gpio_pin_sequence[sequence_last_idx++] = green;
+			break;
+		case 'B':
+			c->gpio_pin_sequence[sequence_last_idx++] = blue;
+			break;
+		}
+	}
+	c->sequence_size = sequence_last_idx;
+}
+
+void switch_on_next_led(led_controller_t *c)
+{
+	if (c->pin_idx >= c->sequence_size)
+	{
+		c->pin_idx = 0;
+	}
+
+	nrf_gpio_pin_write(c->gpio_pin_sequence[c->pin_idx++], SWITCH_LED_ON_SIGNAL);
+}
+
+void switch_off_led(led_controller_t *c)
+{
+	nrf_gpio_pin_write(c->gpio_pin_sequence[c->pin_idx++], SWITCH_LED_OFF_SIGNAL);
+}
+
+void free_led_controller(led_controller_t *c)
+{
+	free(c->gpio_pin_sequence);
+	c->pin_idx = 0;
+	c->sequence_size = 0;
+}
