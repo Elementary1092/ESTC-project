4a035fb965be8a0c911f1e8d084ef3926ad68884
Elementary1092 <pdead6223@gmail.com>
2022-11-14 18:52:52 +0000

    fix: assign uint16 to uint8

--- modules/hsv/hsv_helper.c
+++ modules/hsv/hsv_helper.c
@@ -1,10 +1,19 @@
 #include <nrfx.h>
-#include <math.h>
 #include "hsv_helper.h"
 
 #define HSV_MAX_HUE        360
 #define HSV_MAX_SATURATION 100
 #define HSV_MAX_BRIGHTNESS 100
+
+static uint8_t uint16_to_uint8(uint16_t value)
+{
+	if (value > 255)
+	{
+		return 255;
+	}
+
+	return ( (uint8_t)value );
+}
 
 void hsv_helper_convert(hsv_ctx_t *hsv, rgb_value_t *res)
 {
@@ -15,45 +24,49 @@ void hsv_helper_convert(hsv_ctx_t *hsv, rgb_value_t *res)
 	NRFX_ASSERT(hsv->brightness <= HSV_MAX_BRIGHTNESS);
 
 	uint16_t M = ( 255 * ( (uint16_t)hsv->brightness ) ) / 100;
-	uint16_t m = M * (100 - ( (uint16_t)hsv->saturation) ) / 100;
-	uint16_t hue_mod_2 = ( (hsv->hue) / 60 ) % 2;
+	uint16_t m = M * ( 100 - ( (uint16_t)hsv->saturation ) ) / 100;
+	uint16_t hue_mod_2 = ( hsv->hue / 60 ) % 2;
 	uint16_t z = (M - m) * hue_mod_2;
+
+	uint8_t shade0 = uint16_to_uint8(M);
+	uint8_t shade1 = uint16_to_uint8(z + m);
+	uint8_t shade2 = uint16_to_uint8(m);
 
 	uint16_t hue = hsv->hue;
 	if (hue < 60 || hue == 360)
 	{
-		res->red = M;
-		res->green = z + m;
-		res->blue = m;
+		res->red = shade0;
+		res->green = shade1;
+		res->blue = shade2;
 	}
 	else if (hue < 120)
 	{
-		res->red = z + m;
-		res->green = M;
-		res->blue = m;
+		res->red = shade1;
+		res->green = shade0;
+		res->blue = shade2;
 	}
 	else if (hue < 180)
 	{
-		res->red = m;
-		res->green = M;
-		res->blue = z + m;
+		res->red = shade2;
+		res->green = shade0;
+		res->blue = shade1;
 	}
 	else if (hue < 240)
 	{
-		res->red = m;
-		res->green = z + m;
-		res->blue = M;
+		res->red = shade2;
+		res->green = shade1;
+		res->blue = shade0;
 	}
 	else if (hue < 300)
 	{
-		res->red = z + m;
-		res->green = m;
-		res->blue = M;
+		res->red = shade1;
+		res->green = shade2;
+		res->blue = shade0;
 	}
 	else
 	{
-		res->red = M;
-		res->green = m;
-		res->blue = z + m;
+		res->red = shade0;
+		res->green = shade2;
+		res->blue = shade1;
 	}
 }
