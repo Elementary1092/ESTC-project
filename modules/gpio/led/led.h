e8fb47bc2efbf6a389c7ee3ec9275b00d52dbe0c
Elementary1092 <pdead6223@gmail.com>
2022-11-14 16:56:30 +0000

    ref: extract init leds from sequence init

---
+++ modules/gpio/led/led.h
@@ -0,0 +1,14 @@
+#ifndef APP_LED_H
+#define APP_LED_H
+
+#define LED_SWITCH_ON_SIGNAL  0
+#define LED_SWITCH_OFF_SIGNAL 1
+
+#define LED_PCA10059_RED    NRF_GPIO_PIN_MAP(0, 8)
+#define LED_PCA10059_GREEN  NRF_GPIO_PIN_MAP(1, 9)
+#define LED_PCA10059_BLUE   NRF_GPIO_PIN_MAP(0, 12)
+#define LED_PCA10059_YELLOW NRF_GPIO_PIN_MAP(0, 6)
+
+void led_init_all(void);
+
+#endif
