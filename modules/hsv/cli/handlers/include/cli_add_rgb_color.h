adf99d0f4f69039ba4c2f0ab5220499a57087424
Elementary1092 <pdead6223@gmail.com>
2023-02-17 17:58:09 +0000

    [WIP][RFR] Change hsv_cli_handler_add_rgb_color Return Type

--- modules/hsv/cli/handlers/include/cli_add_rgb_color.h
+++ modules/hsv/cli/handlers/include/cli_add_rgb_color.h
@@ -25,9 +25,9 @@ hsv_cli_handler_i hsv_cli_add_rgb_color(void);
  *       - BLUE;
  *       - COLOR_NAME.
 */
-void hsv_cli_handler_add_rgb_color(app_usbd_cdc_acm_t const *cdc_acm, 
-                                   char args[][HSV_CLI_MAX_WORD_SIZE], 
-    						       uint8_t nargs);
+estc_cli_error_t hsv_cli_handler_add_rgb_color(app_usbd_cdc_acm_t const *cdc_acm, 
+                                               char args[][HSV_CLI_MAX_WORD_SIZE], 
+    						                   uint8_t nargs);
 
 /**
  * @brief Returns add_rgb_color command help prompt.
