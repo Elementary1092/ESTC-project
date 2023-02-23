504449dfc4169a33405b855154089864d90ca417
Elementary1092 <pdead6223@gmail.com>
2023-02-23 07:21:11 +0000

    [WIP][RFR] Change hsv_cli_exec_help Such That It Can Be Result Describer

--- modules/hsv/cli/cli_handlers.c
+++ modules/hsv/cli/cli_handlers.c
@@ -8,6 +8,11 @@
 #include "hsv_picker.h"
 #include "hsv_saved_colors.h"
 
+#ifndef ESTC_CLI_HELP_HEADER
+#define ESTC_CLI_HELP_HEADER "List of available commands:\r\n" \
+                             "help - print information about available commands\r\n"
+#endif
+
 static hsv_cli_command_handler_get command_handlers[] =
 {
 	HSV_CLI_HANDLERS(HSV_CLI_EXECUTORS_EXPAND_AS_INTERFACE)
@@ -25,26 +30,35 @@ static const char *hsv_cli_get_cmd_str(hsv_cli_command_t h)
 	return handler.command_name();
 }
 
-static void hsv_cli_exec_help(estc_cli_error_t error, char buffer[512])
+static void hsv_cli_exec_help(estc_cli_error_t error, char help_prompt[512])
 {
-	char help_prompt[512] = "List of available commands:\r\n"
-							"help - print information about available commands\r\n";		
-	int max_len = sizeof(help_prompt) - strlen(help_prompt) - 1;
+	memset(help_prompt, 0, 512);
+	strncat(help_prompt, ESTC_CLI_HELP_HEADER, 511);
+	size_t max_len = 511UL - strlen(help_prompt);
 
 
-	for (int i = 0; i < HSV_CLI_COMMAND_HELP && max_len > 0; i++)
+	for (int i = 0; i < HSV_CLI_COMMAND_HELP; i++)
 	{
 		hsv_cli_handler_i handler = (*command_handlers[i])();
 		const char *command_prompt = handler.help();
 
+		if (max_len < strlen(command_prompt))
+		{
+			break;
+		}
+
 		strncat(help_prompt, command_prompt, max_len);
 		max_len -= strlen(command_prompt);
+
+		if (max_len < 3UL)
+		{
+			break;
+		}
+
 		strncat(help_prompt, "\r\n", max_len);
 		max_len -= 2;
 	}
-	help_prompt[sizeof(help_prompt) - 1] = '\0';
-
-	memcpy(buffer, help_prompt, 512);
+	help_prompt[511] = '\0';
 }
 
 hsv_cli_command_t hsv_cli_resolve_command(const char *command)
