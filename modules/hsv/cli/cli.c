356849387a65cd8d66e4127244c4c19ccd6ce8b6
Elementary1092 <pdead6223@gmail.com>
2023-02-23 07:18:15 +0000

    [WIP][RFR] Change hsv_cli_exec_command return type and remove app_usbd_cdc_acm from arguments

--- modules/hsv/cli/cli.c
+++ modules/hsv/cli/cli.c
@@ -16,18 +16,20 @@
 #define HSV_CLI_MAX_WORD_SIZE ESTC_MAX_LINE_SIZE
 #endif
 
-estc_cli_error_t hsv_cli_exec_command(app_usbd_cdc_acm_t const *cdc_acm,
-						  cdc_acm_read_buf_t *read_buf)
+estc_cli_described_result_t hsv_cli_exec_command(cdc_acm_read_buf_t *read_buf)
 {
 	char args[HSV_CLI_MAX_ARGS + 1][HSV_CLI_MAX_WORD_SIZE] = {0};
 	size_t nargs = utils_strings_tokenize(args, read_buf->buf, " \r\n\t\0");
 
 	if (nargs == 0)
 	{
-		return ESTC_CLI_ERROR_EMPTY_COMMAND;
+		return (estc_cli_described_result_t) {
+			.error = ESTC_CLI_ERROR_EMPTY_COMMAND,
+			.result_describer = estc_cli_describe_error,
+		};
 	}
 	
 	hsv_cli_command_t cmd = hsv_cli_resolve_command(args[0]);
 	
-	return hsv_cli_exec_handler(cmd, cdc_acm, args + 1, nargs - 1);
+	return hsv_cli_exec_handler(cmd, args + 1, nargs - 1);
 }
