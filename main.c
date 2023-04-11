7183a642207c42b79ca8e05a3f3c49cc68025fb8
Elementary1092 <pdead6223@gmail.com>
2023-04-11 14:21:05 +0000

    [RFR] Extract gap peripheral connection from main

--- main.c
+++ main.c
@@ -34,16 +34,17 @@
 #include "modules/ble/service/characteristic.h"
 #include "modules/ble/gap/advertising.h"
 #include "modules/ble/ble_helpers.h"
+#include "modules/ble/gap/estc_gap.h"
 #include "utils/generator/fcyclic_variable.h"
 #include "utils/generator/sinusoid.h"
 
 #define DEVICE_NAME "SomeLongName"              /**< Extended name of a device. */
 #define MANUFACTURER_NAME "NordicSemiconductor" /**< Manufacturer. Will be passed to Device Information Service. */
 
-#define MIN_CONN_INTERVAL MSEC_TO_UNITS(100, UNIT_1_25_MS) /**< Minimum acceptable connection interval (0.1 seconds). */
-#define MAX_CONN_INTERVAL MSEC_TO_UNITS(200, UNIT_1_25_MS) /**< Maximum acceptable connection interval (0.2 second). */
-#define SLAVE_LATENCY 0                                    /**< Slave latency. */
-#define CONN_SUP_TIMEOUT MSEC_TO_UNITS(4000, UNIT_10_MS)   /**< Connection supervisory timeout (4 seconds). */
+#define MIN_CONN_INTERVAL 100U /**< Minimum acceptable connection interval (0.1 seconds). */
+#define MAX_CONN_INTERVAL 200U /**< Maximum acceptable connection interval (0.2 second). */
+#define SLAVE_LATENCY     0    /**< Slave latency. */
+#define CONN_SUP_TIMEOUT 4000U /**< Connection supervisory timeout (4 seconds). */
 
 #define ESTC_NOTIFIED_VALUE_DELAY APP_TIMER_TICKS(2500)
 #define ESTC_INDICATED_VALUE_DELAY APP_TIMER_TICKS(3500)
@@ -149,28 +150,16 @@ static void timers_init(void)
  */
 static void gap_params_init(void)
 {
-    ret_code_t err_code;
-    ble_gap_conn_params_t gap_conn_params;
-    ble_gap_conn_sec_mode_t sec_mode;
-
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
-
-    err_code = sd_ble_gap_device_name_set(&sec_mode,
-                                          (const uint8_t *)DEVICE_NAME,
-                                          strlen(DEVICE_NAME));
-    APP_ERROR_CHECK(err_code);
-
-    err_code = sd_ble_gap_appearance_set(BLE_APPEARANCE_UNKNOWN);
-    APP_ERROR_CHECK(err_code);
-
-    memset(&gap_conn_params, 0, sizeof(gap_conn_params));
-
-    gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
-    gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
-    gap_conn_params.slave_latency = SLAVE_LATENCY;
-    gap_conn_params.conn_sup_timeout = CONN_SUP_TIMEOUT;
-
-    err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
+    estc_ble_gap_config_t config = 
+    {
+        .device_name = (const uint8_t *)DEVICE_NAME,
+        .device_name_len = strlen(DEVICE_NAME),
+        .min_conn_interval_ms = MIN_CONN_INTERVAL,
+        .max_conn_interval_ms = MAX_CONN_INTERVAL,
+        .slave_latency = SLAVE_LATENCY,
+        .conn_supplement_timeout_ms = CONN_SUP_TIMEOUT,
+    };
+    ret_code_t err_code = estc_ble_gap_peripheral_init(config);
     APP_ERROR_CHECK(err_code);
 }
 
